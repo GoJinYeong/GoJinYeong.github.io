4a4e290300afc8009897c985e82f13710e4c3361
GoJinYeong <gtjy55@gmail.com>
2022-06-14 14:48:14 +0900

    임의로 입력 받은 개수의 합과 평균

---
+++ 5장(반복문)/ex13.py
@@ -0,0 +1,21 @@
+# 사용자로부터 임의의 개수의 성적을 입력받아 합계와 평균을 계산
+# 센티넬은 음수 값
+
+sum = 0
+avg = 0
+cnt = 0
+num = 0
+
+print("입력이 끝나면 음수를 입력하세요.")
+
+while num >= 0:
+    num = int(input("과목의 점수를 입력하세요:"))
+
+    if num >= 0:
+        cnt = cnt + 1
+        sum += num
+        avg = sum / cnt
+
+print("과목의 개수: %d" % cnt)
+print("과목의 합: %d" % sum)
+print("과목의 평균: %0.2f" % avg)
